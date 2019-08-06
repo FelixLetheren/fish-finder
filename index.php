d0628dce567902697bb44377839ae5894044e10c
Felix <felix.letheren@gmail.com>
2019-08-06 13:24:00 +0100

    Funtion written to write cards of fish from database.

--- index.php
+++ index.php
@@ -11,9 +11,23 @@ $sql->execute();
 
 $collection = $sql->fetchAll();
 
+function makeCard($fish){
+    foreach( $fish as $fish) {
+        echo '<div class="fish-card">';
+        echo '<h2 class="name">' . $fish['name'] . '</h2>';
+        echo '<h3 class="stat">' . $fish['species'] . '</h3>';
+        echo '<img alt="fish picture" class="fish-picture" src="' . $fish['img-filepath'] . '">';
+        echo '<h3 class="stat"> Length- ' . $fish['length'] . '</h3>';
+        echo '<h3 class="stat"> Aggression- ' . $fish['aggression'] . '</h3>';
+        echo '<h3 class="stat"> Colour- ' . $fish['colour'] . '</h3>';
+        echo '<h3 class="stat"> Pattern- ' . $fish['pattern'] . '</h3>';
+        echo '</div>';
+    }
+}
 ?>
 <html lang="en">
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <link href="stylesheets/normalise.css" rel="stylesheet" type="text/css">
     <link href="stylesheets/stylesheet.css" rel="stylesheet" type="text/css">
@@ -24,33 +38,8 @@ $collection = $sql->fetchAll();
     <h1>Fish Finder</h1>
 </div>
 <div class="row-container">
-<div class="fish-card">
-    <h2 class="name"><?php echo $collection[0]['name']; ?></h2>
-    <h2 class="stat"><?php echo $collection[0]['species']; ?></h2>
-    <img class="fish-picture" src="<?php echo $collection[0]['img-filepath'];?>">
-    <h2 class="stat">Length- <?php echo $collection[0]['length']; ?></h2>
-    <h2 class="stat">Agression- <?php echo $collection[0]['aggression']; ?></h2>
-    <h2 class="stat">Colour- <?php echo $collection[0]['color']; ?></h2>
-    <h2 class="stat">Pattern- <?php echo $collection[0]['pattern']; ?></h2>
+    <?php makeCard($collection);?>
 </div>
-    <div class="fish-card">
-        <h2 class="name"><?php echo $collection[1]['name']; ?></h2>
-        <h2 class="stat"><?php echo $collection[1]['species']; ?></h2>
-        <img class="fish-picture" src="<?php echo $collection[1]['img-filepath'];?>">
-        <h2 class="stat"><?php echo $collection[1]['length']; ?></h2>
-        <h2 class="stat"><?php echo $collection[1]['aggression']; ?></h2>
-        <h2 class="stat"><?php echo $collection[1]['color']; ?></h2>
-        <h2 class="stat"><?php echo $collection[1]['pattern']; ?></h2>
-    </div>
-    <div class="fish-card">
-        <h2 class="name"><?php echo $collection[2]['name']; ?></h2>
-        <h2 class="stat"><?php echo $collection[2]['species']; ?></h2>
-        <img class="fish-picture" src="<?php echo $collection[2]['img-filepath'];?>">
-        <h2 class="stat"><?php echo $collection[2]['length']; ?></h2>
-        <h2 class="stat"><?php echo $collection[2]['aggression']; ?></h2>
-        <h2 class="stat"><?php echo $collection[2]['color']; ?></h2>
-        <h2 class="stat"><?php echo $collection[2]['pattern']; ?></h2>
-    </div>
 </div>
 </body>
 </html>
