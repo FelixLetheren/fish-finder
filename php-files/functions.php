7d3fb9ada591c096932c4c7324bd139080c5e482
Felix <felix.letheren@gmail.com>
2019-08-07 15:07:14 +0100

    Adjusted db functions so PDO:FETCH_ASSOC is always set.

--- php-files/functions.php
+++ php-files/functions.php
@@ -6,6 +6,7 @@
 function databaseConnect()
 {
     $db = new PDO('mysql:host=192.168.20.20; dbname=fish-finder', 'root', '');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
 
@@ -16,7 +17,6 @@ function databaseConnect()
  */
 function getFish($db): array
 {
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $sql = $db->prepare('SELECT `fish`.`name`,`fish`.`species`,`fish`.`img-filepath`,`fish`.`length`,`fish`.`aggression`,`fish`.`color`,`fish`.`pattern` FROM `fish`;');
     $sql->execute();
     $collection = $sql->fetchAll();
