57a166676b8507cd5f22a00b73fb80766c9a5d14
Felix <felix.letheren@gmail.com>
2019-08-07 10:25:35 +0100

    Added more unit tests for failure and malformed. Added checks on displayFish() to make sure necessary fields are set Seperated out functions into seperate file

--- index.php
+++ index.php
@@ -1,50 +1,7 @@
 <!DOCTYPE html>
 <?php
-/**
- * @return PDO
- * Connects to database
- */
-function databaseConnect(){
-    $db = new PDO('mysql:host=192.168.20.20; dbname=fish-finder', 'root', '');
-    return $db;
-}
+require_once 'php-files/functions.php';
 
-/**
- * @param $db
- * @return array
- * Queries the database to get multidimensional array of all data required to display.
- */
-function getFish($db):array
-{
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $sql = $db->prepare('SELECT `fish`.`name`,`fish`.`species`,`fish`.`img-filepath`,`fish`.`length`,`fish`.`aggression`,`fish`.`color`,`fish`.`pattern` FROM `fish`;');
-    $sql->execute();
-    $collection = $sql->fetchAll();
-    return $collection;
-}
-
-/**
- * @param array $fishFromDB
- * @return string
- * Iterates through each associated array and concatenates all the information into one large string
- * of HTML
- */
-function displayFish(array $fishFromDB)
-{
-    $result = '';
-    foreach ($fishFromDB as $fish) {
-        $result .= '<div class="fish-card">';
-        $result .= '<h2 class="name">' . $fish['name'] . '</h2>';
-        $result .= '<h3 class="stat">' . $fish['species'] . '</h3>';
-        $result .= '<img alt="fish picture" class="fish-picture" src="' . $fish['img-filepath'] . '">';
-        $result .= '<h3 class="stat"> Length- ' . $fish['length'] . 'cm</h3>';
-        $result .= '<h3 class="stat"> Aggression- ' . $fish['aggression'] . '</h3>';
-        $result .= '<h3 class="stat"> Colour- ' . $fish['color'] . '</h3>';
-        $result .= '<h3 class="stat"> Pattern- ' . $fish['pattern'] . '</h3>';
-        $result .= '</div>';
-    }
-    return $result;
-}
 $db = databaseConnect();
 $collection = getFish($db);
 ?>
@@ -61,22 +18,7 @@ $collection = getFish($db);
     <h1>Fish Finder</h1>
 </div>
 <div class="container">
-<?php echo displayFish($collection);
-$exampleFish = [
-    ['name' => 'Borris',
-        'species' => 'shark',
-        'img-filepath' => 'images/borris.png',
-        'length' => 7,
-        'aggression' => 2,
-        'color' => 'Blue',
-        'pattern' => 'Spotty'],['name'=>'Borris',
-    'species' => 'shark',
-    'img-filepath' => 'images/borris.png',
-    'length' => 7,
-    'aggression' => 2,
-    'color' => 'Blue',
-    'pattern' => 'Spotty']];
-echo displayFish($exampleFish) ?>
+    <?php echo displayFish($collection); ?>
 </div>
 </body>
 </html>
---
+++ php-files/functions.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @return PDO
+ * Connects to database
+ */
+function databaseConnect()
+{
+    $db = new PDO('mysql:host=192.168.20.20; dbname=fish-finder', 'root', '');
+    return $db;
+}
+
+/**
+ * @param $db
+ * @return array
+ * Queries the database to get multidimensional array of all data required to display.
+ */
+function getFish($db): array
+{
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $sql = $db->prepare('SELECT `fish`.`name`,`fish`.`species`,`fish`.`img-filepath`,`fish`.`length`,`fish`.`aggression`,`fish`.`color`,`fish`.`pattern` FROM `fish`;');
+    $sql->execute();
+    $collection = $sql->fetchAll();
+    return $collection;
+}
+
+/**
+ * @param array $fishFromDB
+ * @return string
+ * Iterates through each associated array and concatenates all the information into one large string
+ * of HTML
+ */
+function displayFish(array $fishFromDB):string
+{
+    $result = '';
+    foreach ($fishFromDB as $fish) {
+        if (isset($fish['name'])
+            && isset($fish['species'])
+            && isset($fish['length'])
+            && isset($fish['aggression'])
+            && isset($fish['color'])) {
+            $result .= '<div class="fish-card">';
+            $result .= '<h2 class="name">' . $fish['name'] . '</h2>';
+            $result .= '<h3 class="stat">' . $fish['species'] . '</h3>';
+            $result .= '<img alt="fish picture" class="fish-picture" src="' . $fish['img-filepath'] . '">';
+            $result .= '<h3 class="stat"> Length- ' . $fish['length'] . 'cm</h3>';
+            $result .= '<h3 class="stat"> Aggression- ' . $fish['aggression'] . '</h3>';
+            $result .= '<h3 class="stat"> Colour- ' . $fish['color'] . '</h3>';
+            $result .= '<h3 class="stat"> Pattern- ' . $fish['pattern'] . '</h3>';
+            $result .= '</div>';
+        } else {
+            return 'Error! please contact administrator';}
+    }
+    return $result;
+}
