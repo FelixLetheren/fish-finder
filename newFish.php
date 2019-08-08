620847c251410a5e2a0fc3df4483e8e8ecac0c52
Felix <felix.letheren@gmail.com>
2019-08-08 11:09:38 +0100

    Added icon on index that links to new fish entry page.

--- newFish.php
+++ newFish.php
@@ -1,9 +1,5 @@
 <?php
-require_once 'php-files/functions.php';
 
-$db = databaseConnect();
-$collection = getFish($db);
-$fishDataHtml = displayFish($collection);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,19 +8,14 @@ $fishDataHtml = displayFish($collection);
     <meta charset="UTF-8">
     <link href="stylesheets/normalise.css" rel="stylesheet" type="text/css">
     <link href="stylesheets/stylesheet.css" rel="stylesheet" type="text/css">
-    <title>Fish Finder</title>
+    <title>Fish Finder- New Fish</title>
 </head>
 <body>
 <div class="title-container">
-    <h1>Fish Finder</h1>
+    <h1>Add a new fish</h1>
 </div>
 <div class="content-container">
-    <?php echo $fishDataHtml; ?>
-</div>
-<div class="link-container">
-    <a class="new-fish-icon" href="newFish.php">
-        <img src="images/plus-icon.png" alt="Add a new fish"
-    </a>
+
 </div>
 </body>
 </html>
