f289eb92be5ef6b732f3be05e2881fee4f669458
Felix <felix.letheren@gmail.com>
2019-08-08 11:29:32 +0100

    Created basic HTML form

--- newFish.php
+++ newFish.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'php-files/functions.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +15,24 @@
     <h1>Add a new fish</h1>
 </div>
 <div class="content-container">
-
+<form method="post" action="newFish.php">
+    <input type="text" placeholder="Name" name="name" required>
+    <input type="text" placeholder="Species" name="species" required>
+    <input type="text" placeholder="Length" name="length" required>
+    <select name="aggression" required>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    <input type="text" placeholder="Pattern" name="pattern" required>
+    <input type="submit" value="Submit" required>
+</form>
 </div>
+<div class="link-container">
+    <a class="new-fish-icon" href="index.php">
+        <img src="images/return.png" alt="Add a new fish"
+    </a>
 </body>
 </html>
